186c5f4747079a8a83e865afa563b4476a29c4f7
akal nakarmi <akalnakarmi1@gmail.com>
2024-04-05 21:40:16 +0545

    render

---
+++ e1/index.js
@@ -0,0 +1,10 @@
+ReactDOM.render(<h1>Hello World</h1>,
+	document.getElementById("root")
+);
+ReactDOM.render(
+	<h1>
+		<li>Thing1</li>
+		<li>Thing2</li>
+	</h1>,
+	document.getElementById("root1")
+);
