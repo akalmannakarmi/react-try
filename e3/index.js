0a01d7e744f05e5dcd488efc8aec4f3c0aeb9210
akal nakarmi <akalnakarmi1@gmail.com>
2024-04-05 22:46:03 +0545

    example

---
+++ e3/index.js
@@ -0,0 +1,15 @@
+const content = (
+<div>
+	<img src="react-logo.png" width="100px"></img>
+	<h1>Fun facts about React</h1>
+	<ul>
+		<li>Was first Released in 2013</li>	
+		<li>Was originally created by Jordan Walke</li>	
+		<li>Has over 100K star in github</li>	
+		<li>Is maintained by facebook</li>	
+		<li>Powers thousands of enterprise apps, including mobile apps</li>	
+	</ul>
+</div>
+)
+
+ReactDOM.render(content,document.getElementById("root"));
